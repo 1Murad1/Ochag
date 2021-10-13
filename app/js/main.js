8f73d59d7f1d5c4d58f8600d8a2a981f37110c50
1Murad1 <muradabdulaev1998borec@gmail.com>
2021-10-13 22:53:10 +0300

    map

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,23 @@
-function initMap() {
-    const map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        center: new google.maps.LatLng(49.968208, 36.363152),
-    });
-    const mark = new google.maps.Marker({
-        position: new google.maps.LatLng(49.968208, 36.363152),
-        map: map,
-        title: "г.Харьков, ул.Енакиевская 8"
-    });
-    const info = new google.maps.InfoWindow({
-        content: "<p style='color: red'>Очаг: сетка сварная " +
-            "оцинкованная...</p>"
-    });
-
-    mark.add
-    Listener("click", function () {
-        info.open(map, mark)
-    });
-}
+// function initMap() {
+//     const map = new google.maps.Map(document.getElementById('map'), {
+//         zoom: 12,
+//         center: new google.maps.LatLng(49.968208, 36.363152),
+//     });
+//     const mark = new google.maps.Marker({
+//         position: new google.maps.LatLng(49.968208, 36.363152),
+//         map: map,
+//         title: "г.Харьков, ул.Енакиевская 8"
+//     });
+//     const info = new google.maps.InfoWindow({
+//         content: "<p style='color: red'>Очаг: сетка сварная " +
+//             "оцинкованная...</p>"
+//     });
+//
+//     mark.add
+//     Listener("click", function () {
+//         info.open(map, mark)
+//     });
+// }
 
 $('.slider').slick({
     arrows: false,
